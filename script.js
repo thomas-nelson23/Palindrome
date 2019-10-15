09b111bfa2b82cd4c09a9d1a1c133516267f9a86
thomas-nelson23 <te.nelson993@gmail.com>
2019-10-14 20:45:40 -0500

    attempt at console tree

--- script.js
+++ script.js
@@ -20,3 +20,36 @@ var checkString = function () {
         result.innerHTML = "It's not a palindrome!";
     }
 }
+
+
+// end palindromer, begin random practice stuff
+
+var printTree = function () {
+
+    var character = "";
+    var radioSelection = document.getElementsByName('character');
+    var treesize = document.getElementById('treeSize').value;
+
+
+    //loop to find selected value
+    for (x = 0; x < radioSelection.length; x++) {
+
+        console.log("x:", x);
+
+        if (radioSelection[x].checked) {
+            character = radioSelection[x].value;
+            break;
+        }
+    }
+
+    var string = character;
+
+    for (var q = 0; q < treesize; q++) {
+        var string = whitespace + character;
+        console.log(string);
+        string += character;
+
+        string = " " + string;
+    }
+
+}
